de93ed563dca85e1b41c68d8129644d59a0cec35
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-10-05 18:19:17 -0700

    add blockchain and verification

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,43 @@
 use blockchain::*;
 
 fn main() {
+    let difficulty: u128 = 0x0000ffffffffffffffffffffffffffff;
     let mut block = Block::new(
         0, 
-        0, 
+        now(), 
         vec![0; 32],
         0, 
         String::from("Genesis block!"),
-        0x00000000ffffffffffffffffffffffff
+        difficulty
     );
-
-    println!("{:?}", block);
 
     block.mine();   
 
-    println!("{:?}", block);
+    println!("Mined genesis block {:?}", block);
+
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    for i in 1..=10 {
+        let mut block = Block::new(
+            i, 
+            now(), 
+            last_hash,
+            0, 
+            String::from("Another block"),
+            difficulty
+        );
+    
+        block.mine();   
+        println!("Mined another block {:?}", block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+
+        println!("Verify: {}", blockchain.verify());
+    }
 }
